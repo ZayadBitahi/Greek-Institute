66b31e382190b404fa9978a5ea1d6201cd71a015
ZayadBitahi <ziadbitahi2001@gmail.com>
2025-08-11 21:48:20 +0100

    first commit

--- week_1/day_1/Exercices.py
+++ week_1/day_1/Exercices.py
@@ -45,3 +45,13 @@ print(basket)
 #Exercice 8
 sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
 while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
+finished_sandwiches = []
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    finished_sandwiches.append(current_sandwich)
+print("The following sandwiches have been made:")
+for sandwich in finished_sandwiches:
+    print("I made your:" + sandwich)
+
+
