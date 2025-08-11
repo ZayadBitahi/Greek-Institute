53747423c05e0214f07971f82cd8aad61d454208
ZayadBitahi <ziadbitahi2001@gmail.com>
2025-08-11 22:38:36 +0100

    first commit

---
+++ week_1/day_1/Exercises_XP_Gold.py
@@ -0,0 +1,59 @@
+#exercise 1
+
+month = int(input("Enter a month number (1-12): "))
+
+if month in [3, 4, 5]:
+    season = "Spring"
+elif month in [6, 7, 8]:
+    season = "Summer"
+elif month in [9, 10, 11]:
+    season = "Autumn"
+elif month == 12 or month == 1 or month == 2:
+    season = "Winter"
+else:
+    season = "Come on man, there are only 12 months in a year!"
+
+print("Season:", season)
+
+#exercise 2
+#first part
+for number in range(1, 21):
+    print(number)
+
+#second part
+for even_number in range(1, 21):
+    if even_number % 2 == 0:
+        print(even_number)
+
+
+#exercise 3
+while True:
+    name = input("Enter your name: ")
+    if name == "Zayad":
+        break
+
+#exercise 4
+names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+
+user_name = input("Enter your name(2): ")
+
+if user_name in names:
+    index = names.index(user_name)
+    print(index)
+else:
+    print("Name not found.")
+
+#exercise 5
+
+
+num1 = int(input("Input the 1st number: "))
+num2 = int(input("Input the 2nd number: "))
+num3 = int(input("Input the 3rd number: "))
+
+greatest = num1
+if num2 > greatest:
+    greatest = num2
+if num3 > greatest:
+    greatest = num3
+
+print("The greatest number is:", greatest)
