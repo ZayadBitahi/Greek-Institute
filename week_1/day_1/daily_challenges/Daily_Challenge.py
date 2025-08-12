405d4b0e02605e7a97accfd448fa43228155246f
ZayadBitahi <ziadbitahi2001@gmail.com>
2025-08-12 09:54:03 +0100

    first commit

--- week_1/day_1/daily_challenges/Daily_Challenge.py
+++ week_1/day_1/daily_challenges/Daily_Challenge.py
@@ -2,9 +2,12 @@
 
 number = int(input("Enter a number: "))
 length = int(input("Enter the length: "))
+multiples = [] 
+
 for i in range(1, length + 1):
-     multiples = [number * i]
-     print(multiples)
+    multiples.append(number * i) 
+
+print(multiples)
 
 
 
