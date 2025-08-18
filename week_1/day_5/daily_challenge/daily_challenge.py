e4f282e09e81f667255ffdd06f205451e25a314f
ZayadBitahi <ziadbitahi2001@gmail.com>
2025-08-18 09:56:49 +0100

    first commit

---
+++ week_1/day_5/daily_challenge/daily_challenge.py
@@ -0,0 +1,33 @@
+import math
+
+class Circle:
+    def __init__(self, radius=None, diameter=None):
+        if radius is not None:
+            self.radius = radius
+        elif diameter is not None:
+            self.radius = diameter / 2
+        else:
+            self.radius = 1
+
+    @property
+    def diameter(self):
+        return self.radius * 2
+
+    @property
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+    def __str__(self):
+        return f"Circle with radius {self.radius} and diameter {self.diameter}"
+
+    def __add__(self, other):
+        return Circle(radius=self.radius + other.radius)
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
