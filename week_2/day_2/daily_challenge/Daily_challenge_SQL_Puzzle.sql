3318e94175492240539f06823d9ae8125a839f9e
ZayadBitahi <ziadbitahi2001@gmail.com>
2025-08-22 11:36:54 +0100

    first commit

---
+++ week_2/day_2/daily_challenge/Daily_challenge_SQL_Puzzle.sql
@@ -0,0 +1,12 @@
+#Q1
+
+The output of the statement is going to be: 
+COUNT(*) = 0
+
+#Q2
+The output of the statement is going to be: 
+COUNT(*) = 2
+
+#Q3
+The output of the statement is going to be: 
+COUNT(*) = 2
